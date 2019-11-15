24879dfcbc15b0cf6229098b65f1176a3e308065
nakai0314 <amuteto.nakai@gmail.com>
2019-11-15 21:03:55 +0900

    変更

--- app/practice/4/nest2.php
+++ app/practice/4/nest2.php
@@ -8,4 +8,9 @@ if($attend === 0){
     echo 'パーティを欠席にて承りました。';
 }elseif($attend === 1){
     echo 'パーティを出席にて承りました。';
+    if($place === 'a'){
+        echo '会場はAホテルでございます。';
+    }elseif($place === 'b'){
+        echo '会場はB広場でございます。';
+    }
 }
