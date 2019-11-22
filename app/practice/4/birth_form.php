42b4fab88b83930d4d5fa5954a56dbb942e8e175
nakai0314 <amuteto.nakai@gmail.com>
2019-11-22 21:13:10 +0900

    変更

---
+++ app/practice/4/birth_form.php
@@ -0,0 +1,20 @@
+<html>
+<body>
+    <h1>生年月日を入力するフォームを作ろう</h1>
+    <label for="year">西暦</label>
+    <select name="year">
+    <?php
+    $now = date("Y");
+    ?>
+    </select>年
+    <br>
+    <select name="month">
+    <option value="1">1</option>
+    <option value="2">2</option>
+    </select>月
+    <select name="day">day
+    <option value="1">1</option>
+    <option value="1">1</option>
+    </select>日
+</body>
+</html>
